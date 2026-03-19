9624cc6e562953f3cab9ba1378662829c7cc9d3b
RaulLiberato <ctt.raul.liberato@gmail.com>
2026-03-19 15:12:13 -0300

    feat: add session thank you animation

--- script.js
+++ script.js
@@ -10,27 +10,41 @@ ScrollSmoother.create({
 gsap.from(".hero", {
     opacity: 0,
     duration: 1,
-  });
+});
 
 gsap.from("picture:nth-child(2)", {
-y: 60,
-duration: 1,
+    y: 60,
+    duration: 1,
 });
 
 gsap.from("picture:nth-child(1)", {
-y: -60,
-duration: 1,
+    y: -60,
+    duration: 1,
 });
 
 //Card animation 
 gsap.from(".card", {
-opacity: 0,
-filter: "blur(10px)",
-stagger: 0.3,
-scrollTrigger: {
-    trigger: ".cards",
-    start: "0% 80%",
-    end: "100% 70%",
-    scrub: true,
-},
+    opacity: 0,
+    filter: "blur(10px)",
+    stagger: 0.3,
+    scrollTrigger: {
+        trigger: ".cards",
+        start: "0% 80%",
+        end: "100% 70%",
+        scrub: true,
+    },
+});
+
+//Session Thank You animation 
+gsap.from(".secaoObrigado ul li", {
+    opacity: 0,
+    x: 40,
+    filter: "blur(10px)",
+    stagger: 0.1,
+    scrollTrigger: {
+        trigger: ".secaoObrigado ul",
+        start: "0% 80%",
+        end: "100% 50%",
+        scrub: true,
+    },
 });
