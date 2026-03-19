e34f3229411ca283401747337aabb0abd4f3d925
RaulLiberato <ctt.raul.liberato@gmail.com>
2026-03-19 15:06:19 -0300

    feat: add hero animation

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ ScrollSmoother.create({
   smooth: 1.5,
   effects: true,
 });
+
+//Animation Hero
+gsap.from(".hero", {
+    opacity: 0,
+    duration: 1,
+  });
+
+  gsap.from("picture:nth-child(2)", {
+    y: 60,
+    duration: 1,
+  });
+
+  gsap.from("picture:nth-child(1)", {
+    y: -60,
+    duration: 1,
+  });
